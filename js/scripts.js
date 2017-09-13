995aad4fe9dc96a752a88dc35abf16e34eedace7
at and ke <ke@>
2017-09-13 10:58:42 -0700

    Add frontend script to calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,4 @@
-$(function() {
-  jQuery("h1").click(function() {
-    alert("This is a header.");
-  });
-
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  jQuery("img").click(function() {
-    alert("This is an image.");
-  });
-});
+/* Business / backend logic: */
 
 var add = function(number1, number2) {
   return number1 + number2;
@@ -28,7 +16,60 @@ var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, (number2);
-alert(result);
+var modular = function(number1, number2) {
+  return number1 % number2;
+};
+
+/* Front end interface logic: */
+
+$(document).ready(function() {
+  $("button#add").click(function(){
+    $("form#numSubmit").submit(function(event){
+    event.preventDefault();
+    var number1 = parseFloat($("#num1").val());
+    var number2 = parseFloat($("#num2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+    });
+  });
+
+  $("button#sub").click(function(){
+    $("form#numSubmit").submit(function(event){
+    event.preventDefault();
+    var number1 = parseFloat($("#num1").val());
+    var number2 = parseFloat($("#num2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+    });
+  });
+
+  $("button#multi").click(function(){
+    $("form#numSubmit").submit(function(event){
+    event.preventDefault();
+    var number1 = parseFloat($("#num1").val());
+    var number2 = parseFloat($("#num2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+    });
+  });
+
+  $("button#divi").click(function(){
+    $("form#numSubmit").submit(function(event){
+    event.preventDefault();
+    var number1 = parseFloat($("#num1").val());
+    var number2 = parseFloat($("#num2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+    });
+  });
+
+  $("button#modu").click(function(){
+    $("form#numSubmit").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
+    var result = modular(number1, number2);
+    $("#output").text(result);
+    });
+  });
+});
